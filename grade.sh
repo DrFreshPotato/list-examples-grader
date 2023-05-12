01054a44a4fb4e46ccbe8bb8d81771b6cb36cde2
DrFreshPotato <khovandrew@gmail.com>
2023-05-11 20:23:31 -0700

    Lab Week 6

--- grade.sh
+++ grade.sh
@@ -1,11 +1,28 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
+echo $CPATH
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
 git clone $1 student-submission
+
+#Checking correct file submitted
+if [[ -f "student-submission/ListExamples.java" ]]
+then 
+    cp student-submission/ListExamples.java grading-area
+    cp TestListExamples.java grading-area
+    cp -r lib grading-area
+else 
+    echo "ListExamples.java file not found"
+fi
+cd grading-area
+javac ListExamples.java
+javac -cp $CPATH TestListExamples.java
+#Not using $CPATH because command is different on windows than mac/linux
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > results.txt
+grep "Failure" results.txt
+
 echo 'Finished cloning'
 
 
